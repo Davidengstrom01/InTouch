30067e43d7093b7cfd28a395568d8eb657fb7db0
David Engström <davveengstrom01@gmail.com>
2026-08-05 09:59:12 +0200

    Initial Commit

---
+++ InTouch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InTouch
+//
+//  Created by David Engström on 2026-08-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InTouch/InTouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  InTouchApp.swift
+//  InTouch
+//
+//  Created by David Engström on 2026-08-05.
+//
+
+import SwiftUI
+
+@main
+struct InTouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
